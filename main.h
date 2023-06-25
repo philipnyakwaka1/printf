a17232cfbfcbfb934573e11148a438ba40fc07fb
Sgechuki <sgechuki@outlook.com>
2023-06-25 11:32:26 -0700

    Ad struct desc

--- main.h
+++ main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 #include <stdarg.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+
+/**
+ * struct specifier - struct holding specifier
+ * @sp: specifier
+ * @p: function pointer
+ *
+ */
 typedef struct specifier
 {
 	char *sp;
