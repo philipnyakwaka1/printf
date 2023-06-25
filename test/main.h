e18098aed4c33c2c9c3ba8d61c482e13599fc69c
Sgechuki <sgechuki@outlook.com>
2023-06-25 11:48:39 -0700

    Add stdio library

---
+++ test/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+/**
+ * struct specifier - struct holding specifier
+ * @sp: specifier
+ * @p: function pointer
+ *
+ */
+typedef struct specifier
+{
+	char *sp;
+	int (*p)(va_list);
+} spec_s;
+int _fprintf(spec_s spec[],const char * format, va_list ap);
+spec_s spec_func(spec_s spec[],char c);
+int format_c(va_list ap);
+int format_s(va_list ap);
+int rot13(va_list ap);
+
+#endif
