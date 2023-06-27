611b38fff81a14e0cd523f2328caa6a1925ecaf5
Sgechuki <sgechuki@outlook.com>
2023-06-27 07:15:44 -0700

    Revert

--- _fprintf.c
+++ _fprintf.c
@@ -22,28 +22,24 @@ int _fprintf(spec_s spec[], const char *format, va_list ap)
 			counter += _putchar(format[i]);
 			continue;
 		}
-		slice[0] = spec_func(spec, format[i + 1]);
+		i++;
+		slice[0] = spec_func(spec, format[i]);
 		if (slice[0].sp != NULL)
 		{
-			j = slice[0].p(ap);
-			if (j == -1)
-			{
-				return (j);
-			}
-			counter += j;
+			counter += slice[0].p(ap);
 		}
-		if (slice[0].sp == NULL && format[i + 1] == ' ')
+		if (slice[0].sp == NULL)
 		{
-			if (format[i + 1] != '\0')
+			if (format[i] == '%')
+				counter += _putchar(format[i]);
+			if (format[i] != '\0' && format[i] != '%')
+				counter += _putchar(format[--i]);
+			if (format[i] == '\0')
 			{
-				_putchar(format[i]);
-				_putchar(format[i + 1]);
-				counter = counter + 2;
+				counter = -1;
+				break;
 			}
-			else
-				return (-1);
 		}
-		i++;
 	}
 	return (counter);
 }
